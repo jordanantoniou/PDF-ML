d55ff51dc57ecd75ba2bec1f6f26a337018230bb
Jordan Antoniou <jordanantoniou@RW69HXQTYJ.local>
2022-12-12 13:51:25 +0000

    Added styled components capability

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import './App.css';
+import styled from 'styled-components';
+
+const Title = styled.h1`
+  font-size: 70px;
+  font-weight: 600;
+  background-image: linear-gradient(to top left, #000C14, #F8002F);
+  color: transparent;
+  background-clip: text;
+  -webkit-background-clip: text;
+`;
 
 function App() {
   return (
     <div className="App">
-      <div>PDF ML</div>
+      <Title>PDF ML</Title>
     </div>
   );
 }
