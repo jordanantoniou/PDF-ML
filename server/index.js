54b74f6a6d135483a937385795f9146c4cbdb89c
Jordan Antoniou <jordanantoniou@rw69hxqtyj.home>
2022-12-14 22:15:36 +0000

    Added Mongoose capability

--- server/index.js
+++ server/index.js
@@ -1,31 +1,19 @@
 import express from 'express';
 import cors from 'cors';
-import { parsePDF, readFilesFromDirectory } from './helpers/utils.js';
-import { train, predict } from './npl-file-processor.js';
+import predict from './routes/predict.js';
+import restore from './routes/restore.js';
+import { trainModel } from './helpers/npl.js';
+import { connectToDatabase } from './helpers/mongo.js';
 
 const app = express();
 
 app.use(cors());
+app.use(predict);
+app.use(restore);
 
-const confirmationStatements = await readFilesFromDirectory(
-  './test/data/confirmation-statements'
-);
-const other = await readFilesFromDirectory('./test/data/other');
+connectToDatabase();
 
-train(confirmationStatements, other);
-
-app.get('/', async (req, res) => {
-  const confirmationStatement = await parsePDF(
-    './test/data/predict/CS2022.pdf'
-  );
-  const other = await parsePDF('./test/data/predict/GA2021.pdf');
-
-  const confirmationPrediction = predict(confirmationStatement);
-  const otherPrediction = predict(other);
-
-  //TODO: Pass in filename to response object when predicting dynamic file
-  res.send([confirmationPrediction, otherPrediction]);
-});
+await trainModel();
 
 app.listen(8080, async () => {
   console.log('Listening on http://localhost:8080...');
